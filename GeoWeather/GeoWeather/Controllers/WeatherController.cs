9f8177543a0364bb4617e5cb21111e460390ae51
Naveen Kumar K <37833848+knaveen01@users.noreply.github.com>
2021-02-02 19:50:39 +0530

    Source Code initial version

---
+++ GeoWeather/GeoWeather/Controllers/WeatherController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GeoWeather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController: ControllerBase
+    {
+        private IConfiguration configuration;
+        public WeatherController(IConfiguration config)
+        {
+            configuration = config;
+        }
+        // GET api/weather
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "No IP is found"};
+        }
+
+        // GET api/weather/223.237.233.31
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            string weatherApiHost = configuration.GetSection("MyConfig").GetSection("WeatherApiHost").Value;
+            string accessKey = configuration.GetSection("MyConfig").GetSection("AccessKey").Value;
+            var ApiUrl = weatherApiHost+"access_key="+ accessKey + "&query="+ id;
+            var responseString = "";
+            var request = (HttpWebRequest)WebRequest.Create(ApiUrl);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+
+            using (var response1 = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response1.GetResponseStream()))
+                {
+                    responseString = reader.ReadToEnd();
+                }
+            }
+            return responseString;
+        }
+    }
+}
